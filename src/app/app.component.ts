4b97dde96bd78cd39a6df11744b2637ffcb00907
HRana93 <himanshu.rana05@gmail.com>
2021-12-01 15:19:00 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component, VERSION } from '@angular/core';
+// app.component.ts
+
+import { Component } from '@angular/core';
+import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  title = 'Angular Form Validation Tutorial';
+   angForm: FormGroup;
+   constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+   createForm() {
+    this.angForm = this.fb.group({
+       firstName: ['', Validators.required ],
+       email: ['', Validators.required ],
+       lastName: ['', Validators.required ],
+       Male: ['', Validators.required ],
+       Female: ['', Validators.required ],
+       search: ['', Validators.required ],
+       address: ['', Validators.required ],
+       male: ['', Validators.required ],
+       female: ['', Validators.required ]
+    });
+  }
 }
